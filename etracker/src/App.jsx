6515f25b60cc64421760b98afa7ddfe5726ef33c
Alex Popa <alex24popa@yahoo.com>
2022-11-05 21:56:02 +0200

    background

--- etracker/src/App.jsx
+++ etracker/src/App.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import Btn from './components_react/test.jsx';
 import "./App.css";
-import { useEffect, useState } from 'react';
+import "./components_react/Background.css";
+import "./components_react/Second_background.css";
+
 
 
 const App = () => {
     return (
-        <div className="box">
-            <nav>
-                <NavLink to='/Create a account'>
-                    Sign In
-                </NavLink>
-                <NavLink to='Sign in as Admin'>
-                    Sign in as Admin
-                </NavLink>
-                <Navlink to='Sign in as Employee'>
-                    Sign in as Employee
-                </Navlink>
-                <NavLink to='About us'>
-                    About us
-                </NavLink>
-
-            </nav>
-        </div>
+      <div className="background">
+          <div className="second_background">
+          </div>
+      </div>
     );
 };
 

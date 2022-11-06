cadf7420ac0e80e457e698045e444f8c74e1e410
Bildea Cristian <Bildea.Sa.Ioan@student.utcluj.ro>
2022-11-06 08:56:39 +0200

    admin

--- etracker/src/components_react/Admin.jsx
+++ etracker/src/components_react/Admin.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-
+import "./Background.css"
+import "./Dashboard.css"
+import "./Second_background_sign_up.css";
 
 const Admin= ()=>{
-    return(
-        <div>Salut si bine te am gasit!!!</div>
-    )
+
+        return (
+            <div className="background">
+                <div className="second_background">
+                    <div className="dashboard"></div>
+                    <div className="AdminName">
+                        <h2>hi</h2>
+                    </div>
+
+
+                </div>
+            </div>
+        );
+
 
 }
 export default Admin;

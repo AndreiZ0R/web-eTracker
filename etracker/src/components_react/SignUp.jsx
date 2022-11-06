383733e4433b6c2e882b689b357e0657fe92afa2
Alex Popa <alex24popa@yahoo.com>
2022-11-06 03:32:16 +0200

    pam12

---
+++ etracker/src/components_react/SignUp.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./SignUp.css";
+import "./components_react/Background.css"
+import "./components_react/second_background_sign_up.css";
+
+
+
+const SignUp = () => {
+    return (
+        <div className="background">
+            <div className="second_background_sign_up">
+                <div className="Header">
+                    Sign Up
+                </div>
+                <p className="paragraphSignUp">
+                    Company name
+                </p>
+
+            </div>
+        </div>
+    );
+}
